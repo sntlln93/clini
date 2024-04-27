30d7110497640ac8e0a6fbb2252bf0126391488c
Matias Santillan <sntlln.93@gmail.com>
2024-04-27 18:29:17 -0300

    api/Add user type

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
+            $table->enum('type', ['practice', 'patient'])->default('practice');
             $table->string('name');
             $table->string('password');
             $table->rememberToken();
