069ff371a9dcb00f46c7b2987d47e6cdf6ade914
Matias Santillan <sntlln.93@gmail.com>
2024-04-27 23:54:29 -0300

    api/Add Address and Patient entities

---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Patient;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Patient::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'dni' => (string) fake()->numberBetween(10_000, 999_999_999),
+            'names' => fake()->name(),
+            'lastname' => fake()->lastName(),
+            'date_of_birth' => fake()->dateTimeBetween(startDate: 90),
+            'sex' => Arr::random(['F', 'M', 'U']),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::factory(1)->create([
+            'username' => 'Matias',
+        ]);
+
+        $this->call([
+            PatientSeeder::class,
         ]);
     }
 }
