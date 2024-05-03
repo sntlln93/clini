c536cdbfaddb3e24608c3e6661f63d78975c5e04
Matias Santillan <sntlln.93@gmail.com>
2024-05-03 15:58:10 -0300

    Remove key:gen and update migrate command from deploy script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -3,9 +3,6 @@ echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
-php artisan key:generate --show
-
 echo "Caching config..."
 php artisan config:cache
 
@@ -13,4 +10,4 @@ echo "Caching routes..."
 php artisan route:cache
 
 echo "Running migrations..."
-php artisan migrate --force
+php artisan migrate:fresh --seed --force
