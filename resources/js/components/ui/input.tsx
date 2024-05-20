7940f87db8665acfc0eaf0b02fd4e7476403a585
Matias Santillan <sntlln.93@gmail.com>
2024-05-20 14:11:16 -0300

    Client: Add icon to input

--- resources/js/components/ui/input.tsx
+++ resources/js/components/ui/input.tsx
@@ -1,25 +1,95 @@
-import * as React from "react"
+import * as React from "react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { Eye, EyeOff, Lock, LucideIcon, LucideProps } from "lucide-react";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+    extends React.InputHTMLAttributes<HTMLInputElement> {
+    startIcon?: LucideIcon;
+    endIcon?: LucideIcon;
+    iconProps?: LucideProps;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
-    return (
-      <input
-        type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
-        ref={ref}
-        {...props}
-      />
-    )
-  }
-)
-Input.displayName = "Input"
+    (
+        { className, type, startIcon, endIcon, iconProps = {}, ...props },
+        ref
+    ) => {
+        const [show, setShow] = React.useState(false);
+        const StartIcon = startIcon;
+        const EndIcon = endIcon;
+        const { className: iconClassName, ...iconRest } = iconProps;
 
-export { Input }
+        if (type === "password") {
+            return (
+                <div className="w-full relative">
+                    <div className="absolute left-1.5 top-1/2 transform -translate-y-1/2">
+                        <Lock
+                            size={18}
+                            className={cn("opacity-40", iconClassName)}
+                            {...iconRest}
+                        />
+                    </div>
+                    <input
+                        autoComplete="off"
+                        type={!show ? type : "text"}
+                        className={cn(
+                            "flex h-10 w-full rounded-md border border-input bg-background py-2 px-8 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+                            className
+                        )}
+                        ref={ref}
+                        {...props}
+                    />
+                    <button
+                        onClick={() => setShow((prev) => !prev)}
+                        className="absolute right-3 top-1/2 transform -translate-y-1/2"
+                        type="button"
+                    >
+                        {show ? (
+                            <Eye className="opacity-40" size={18} />
+                        ) : (
+                            <EyeOff className="opacity-40" size={18} />
+                        )}
+                    </button>
+                </div>
+            );
+        }
+
+        return (
+            <div className="w-full relative">
+                {StartIcon && (
+                    <div className="absolute left-1.5 top-1/2 transform -translate-y-1/2">
+                        <StartIcon
+                            size={18}
+                            className={cn("opacity-40", iconClassName)}
+                            {...iconRest}
+                        />
+                    </div>
+                )}
+                <input
+                    type={type}
+                    className={cn(
+                        "flex h-10 w-full rounded-md border border-input bg-background py-2 px-4 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring focus-visible:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50",
+                        startIcon ? "pl-8" : "",
+                        endIcon ? "pr-8" : "",
+                        className
+                    )}
+                    ref={ref}
+                    {...props}
+                />
+                {EndIcon && (
+                    <div className="absolute right-3 top-1/2 transform -translate-y-1/2">
+                        <EndIcon
+                            className={cn("opacity-40", iconClassName)}
+                            {...iconRest}
+                            size={18}
+                        />
+                    </div>
+                )}
+            </div>
+        );
+    }
+);
+Input.displayName = "Input";
+
+export { Input };
