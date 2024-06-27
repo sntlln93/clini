cef165c17b80c23fe4a26d026cc81bcc3a11b724
Matias Santillan <sntlln.93@gmail.com>
2024-06-27 05:15:11 -0300

    Client: Hide router devtools in prod

--- resources/js/routes/__root.tsx
+++ resources/js/routes/__root.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { useAuth } from "@/lib/hooks/useAuth";
 import { QueryClient } from "@tanstack/react-query";
 import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import React from "react";
 import { z } from "zod";
 
 type AuthContext = ReturnType<typeof useAuth>;
@@ -10,6 +10,15 @@ type AuthContext = ReturnType<typeof useAuth>;
 const redirectSchema = z.object({
     redirect: z.string().optional(),
 });
+
+const TanStackRouterDevtools =
+    process.env.NODE_ENV === "production"
+        ? () => null // Render nothing in production
+        : React.lazy(() =>
+              import("@tanstack/router-devtools").then((res) => ({
+                  default: res.TanStackRouterDevtools,
+              })),
+          );
 
 export const Route = createRootRouteWithContext<{
     auth: AuthContext;
@@ -19,7 +28,9 @@ export const Route = createRootRouteWithContext<{
         <>
             <Outlet />
             <Toaster />
-            <TanStackRouterDevtools />
+            <React.Suspense>
+                <TanStackRouterDevtools />
+            </React.Suspense>
         </>
     ),
     validateSearch: redirectSchema,
