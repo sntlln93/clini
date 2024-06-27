0b4915bf14c5017077dbe942558131e85c04efd1
Matias Santillan <sntlln.93@gmail.com>
2024-06-27 01:25:24 -0300

    Client: Fix nav item not navigating to page

--- resources/js/components/tabs/index.tsx
+++ resources/js/components/tabs/index.tsx
@@ -7,20 +7,20 @@ export function Tabs() {
     const { pathname } = useLocation();
 
     return (
-        <nav className="bg-primary-foreground fixed bottom-0 flex w-full justify-between z-50 shadow-inner rounded-t-3xl">
+        <nav className="bg-primary-foreground fixed bottom-0 flex w-full justify-between z-50 shadow-inner rounded-t-2xl">
             {links.map((link, index) => (
                 <Link
                     key={link.href}
-                    href={link.href}
+                    to={link.href}
                     className={cn(
                         "flex flex-col flex-1 gap-1 items-center py-3",
                         pathname === link.href
                             ? "bg-primary text-primary-foreground"
                             : "text-primary/80",
                         index + 1 === links.length
-                            ? "rounded-tr-3xl"
+                            ? "rounded-tr-2xl"
                             : "rounded-tr-none",
-                        index === 0 ? "rounded-tl-3xl" : "rounded-tl-none",
+                        index === 0 ? "rounded-tl-2xl" : "rounded-tl-none",
                     )}
                 >
                     <link.icon className="h-4 w-4" />
