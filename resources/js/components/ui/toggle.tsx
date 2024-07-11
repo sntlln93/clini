b8bf1640e168e288381e5423093127268bd990cb
Matias Santillan <sntlln.93@gmail.com>
2024-07-11 05:14:50 -0300

    Client: fix readability on toggle

--- resources/js/components/ui/toggle.tsx
+++ resources/js/components/ui/toggle.tsx
@@ -5,11 +5,11 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const toggleVariants = cva(
-    "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent data-[state=on]:border-slate-300 data-[state=on]:border-2 data-[state=on]:text-accent-foreground",
+    "inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-background transition-colors hover:bg-muted hover:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=on]:bg-accent  data-[state=on]:text-accent-foreground",
     {
         variants: {
             variant: {
-                default: "bg-transparent",
+                default: "border-slate-300 border-2",
                 outline:
                     "border border-input bg-transparent hover:bg-accent hover:text-accent-foreground",
             },
